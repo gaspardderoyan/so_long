700c6741dd5d35c7799a955d7d5de12e0056732b
Gaspard Deroyan <gderoyan@f4r11s9.clusters.42paris.fr>
2025-11-29 00:02:52 +0100

    feat(WIP): added printf files ; need to add/deal with header and makefile

---
+++ libft/ft_printf.c
@@ -0,0 +1,62 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gderoyqn <gderoyqn@student.42london.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/08 13:35:55 by gderoyqn          #+#    #+#             */
+/*   Updated: 2025/01/08 13:35:58 by gderoyqn         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+#include "types.h"
+
+void	choose_fun(va_list *ptr, const char **str, int *ccount)
+{
+	char	type;
+
+	type = *(*str + 1);
+	if (type == 'c')
+		p_char(ptr, ccount);
+	else if (type == 's')
+		p_str(ptr, ccount);
+	else if (type == 'p')
+		p_ptr(ptr, ccount);
+	else if (type == 'i' || type == 'd')
+		p_int(ptr, ccount);
+	else if (type == 'u')
+		p_ubase(ptr, ccount, "0123456789");
+	else if (type == 'x')
+		p_ubase(ptr, ccount, "0123456789abcdef");
+	else if (type == 'X')
+		p_ubase(ptr, ccount, "0123456789ABCDEF");
+	else if (type == '%')
+		p_percent(ccount);
+	else
+		p_else(*str, ccount);
+	(*str)++;
+}
+
+int	ft_printf(const char *str, ...)
+{
+	va_list	ptr;
+	int		ccount;
+
+	ccount = 0;
+	va_start(ptr, str);
+	while (*str)
+	{
+		if (*str != '%')
+		{
+			write(1, str, 1);
+			ccount++;
+		}
+		else if (*(str + 1))
+			choose_fun(&ptr, &str, &ccount);
+		str++;
+	}
+	va_end(ptr);
+	return (ccount);
+}
---
+++ libft/helpers.c
@@ -0,0 +1,68 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   helpers.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gderoyqn <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/14 01:07:56 by gderoyqn          #+#    #+#             */
+/*   Updated: 2024/12/14 01:08:20 by gderoyqn         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "helpers.h"
+
+size_t	strlen_safe(const char *s)
+{
+	int	i;
+
+	i = 0;
+	if (!s)
+		return (0);
+	while (s[i])
+		i++;
+	return (i);
+}
+
+static int	ft_nbrlen_base(unsigned long long n, int base)
+{
+	int				len;
+
+	len = 0;
+	if (n <= 0)
+	{
+		len = 1;
+		n = -n;
+	}
+	while (n != 0)
+	{
+		n /= base;
+		len++;
+	}
+	return (len);
+}
+
+char	*ft_itoa_base(unsigned long long n, char *base)
+{
+	int		base_len;
+	int		len;
+	char	*res;
+
+	base_len = strlen_safe(base);
+	len = ft_nbrlen_base(n, base_len);
+	res = (char *)malloc(sizeof(char) * (len + 1));
+	if (!res)
+		return (NULL);
+	res[len] = '\0';
+	if (n == 0)
+	{
+		res[0] = '0';
+		return (res);
+	}
+	while (n != 0)
+	{
+		res[--len] = *((n % base_len) + base);
+		n /= base_len;
+	}
+	return (res);
+}
