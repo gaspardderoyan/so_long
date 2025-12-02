575f3c3a4c11bfffb21781cef67e5e0133f8c5d2
Gaspard Deroyan <gderoyan@f4r1s10.clusters.42paris.fr>
2025-12-02 19:37:01 +0100

    chore: normed the header

--- includes/so_long.h
+++ includes/so_long.h
@@ -6,7 +6,7 @@
 /*   By: gderoyan <gderoyan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/21 14:59:18 by gderoyan          #+#    #+#             */
-/*   Updated: 2025/12/02 19:24:42 by gderoyan         ###   ########.fr       */
+/*   Updated: 2025/12/02 19:36:35 by gderoyan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,18 +24,18 @@
 
 typedef enum e_tex_id
 {
-    TEX_WALL,
-    TEX_FLOOR,
-    TEX_PLAYER,
-    TEX_COLL,
-    TEX_EXIT,
-    TEX_P_EXIT
-}   t_tex_id;
+	TEX_WALL,
+	TEX_FLOOR,
+	TEX_PLAYER,
+	TEX_COLL,
+	TEX_EXIT,
+	TEX_P_EXIT
+}	t_tex_id;
 
 typedef struct s_pos {
 	int	x;
 	int	y;
-}	t_pos ;
+}	t_pos;
 
 typedef struct s_mlx_data {
 	void	*img;
@@ -45,7 +45,7 @@ typedef struct s_mlx_data {
 	int		endian;
 	int		width;
 	int		height;
-} t_mlx_data;
+}	t_mlx_data;
 
 typedef struct s_textures {
 	t_mlx_data	wall;	
@@ -54,32 +54,31 @@ typedef struct s_textures {
 	t_mlx_data	collectible;	
 	t_mlx_data	floor;	
 	t_mlx_data	p_e_floor;	
-} t_textures ;
+}	t_textures;
 
 typedef struct s_map {
-	char	**map;
-	char	**map_copy;
-	t_list	*map_lst;
-	size_t	width;
-	size_t	height;
-	size_t	coin_total;
-	size_t	coin_count;
-	size_t	coin_current;
-	size_t	exit_total;
-	size_t	exit_count;
-	size_t	player_count;
-	size_t	moves;
-	t_pos	position;
-	t_pos	exit;
-	void	*mlx;
-	void	*win;
+	char		**map;
+	char		**map_copy;
+	t_list		*map_lst;
+	size_t		width;
+	size_t		height;
+	size_t		coin_total;
+	size_t		coin_count;
+	size_t		coin_current;
+	size_t		exit_total;
+	size_t		exit_count;
+	size_t		player_count;
+	size_t		moves;
+	t_pos		position;
+	t_pos		exit;
+	void		*mlx;
+	void		*win;
 	t_textures	textures;
-}	t_map ;
-
+}	t_map;
 
 /* utils_str */
 size_t	ft_strlen_safe(char *str);
-int	ft_strcmp(char *s1, char *s2);
+int		ft_strcmp(char *s1, char *s2);
 size_t	ft_strspn(const char *s, const char *accept);
 size_t	get_chars_count(const char *str, char *chars);
 
@@ -102,7 +101,7 @@ void	init_struct(t_map *map);
 void	load_all_textures(t_map *map);
 
 /* cleanup */
-int	close_game(t_map *map);
+int		close_game(t_map *map);
 void	exit_error(char *msg, t_map *map);
 
 /* render */
@@ -110,6 +109,6 @@ void	render_sprite(t_map *m, t_pos pos, t_tex_id type);
 void	render_map(t_map *map);
 
 /* events */
-int	key_press_handler(int keycode, t_map *map);
+int		key_press_handler(int keycode, t_map *map);
 
 #endif
